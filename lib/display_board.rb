c1ef4ae6a2ea95df28dc43d180706b7e2cba64be
Alexander Findlay <info@coded.media>
2017-08-18 17:51:09 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(values)
+  line       = "-----------"
+  row_one    = " #{values[0]} | #{values[1]} | #{values[2]} "
+  row_two    = " #{values[3]} | #{values[4]} | #{values[5]} "
+  row_three  = " #{values[6]} | #{values[7]} | #{values[8]} "
+  puts row_one, line, row_two, line, row_three
+end
